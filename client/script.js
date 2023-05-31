49bfee0b87853739ca0fb778170f9b74f61b746f
Shamil <shamilvkage@gmail.com>
2023-05-31 19:31:57 +0530

    port change

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:3000', {
+    const response = await fetch('http://localhost:5060', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
