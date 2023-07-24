cad0ae1676aafbacd0bfcf8ed3570a431798f7df
Shamil <shamilvkage@gmail.com>
2023-07-24 16:47:50 +0530

    msg update

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'HEY THERE, AI WELCOMES YOU!...'
+    message: 'Hello , AI WELCOMES YOU!...'
   });
 });
 
